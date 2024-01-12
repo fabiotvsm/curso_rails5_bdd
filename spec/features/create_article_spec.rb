04c37545a0b3e091c1c31a07224d20030fd03016
FABIO TIAGO <fabiotvsm@hotmail.com>
2024-01-12 10:03:44 -0300

    adicionando validação

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -14,4 +14,23 @@ RSpec.feature "Create Article" do
         expect(page).to have_content("The article has been created")
         expect(page.current_path).to eq(articles_path)
     end
+
+    scenario "The user fails to create a article" do
+        visit "/"
+
+        click_link "New Article"
+
+        fill_in "Title", with:""
+        fill_in "Body", with:""
+
+
+        click_button "Create Article"
+
+        expect(page).to have_content("Article has not been created")
+        expect(page).to have_content("Title can't be blank")
+        expect(page).to have_content("Body can't be blank")
+        # expect(page.current_path).to eq(articles_path)
+
+    end
+  
 end
