763de3f96fd91ca1c49cd227599f94625f793af3
FABIO TIAGO <fabiotvsm@hotmail.com>
2024-01-19 12:13:55 -0300

    Add users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'users/:id' , to: 'users#main', as: 'user'
+  resources :users, except: %i[show new create destroy]
   devise_for :users
   # get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
